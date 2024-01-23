dd9d0ed4e0bc8f713e7ae665a79b9253448f5d2c
wyx <wuyuxiang.wyx@alibaba-inc.com>
2024-01-23 17:49:57 +0800

    ⚰️remove invert mask fill code

--- src/components/Mask/index.jsx
+++ src/components/Mask/index.jsx
@@ -2,30 +2,19 @@ import React, { isValidElement, useMemo } from "react"
 import Svg, { Circle, Defs, Mask, Rect, Text, Use } from "react-native-svg";
 import tinycolor from "tinycolor2";
 import { isNil } from "lodash";
-// import { invertColor } from "~/utils";
 
 export default function (props = {}) {
   const { children, maskFill, maskOpacity } = props;
   const fill = useMemo(() => {
-    // 需要将配置的颜色反转，以符合直觉上的蒙版背景色
     if (!maskFill) {
       return "#ffffff73";
     }
-    // const invertedColor = tinycolor(maskFill);
     const originColor = tinycolor(maskFill);
-    // const originOpacity = originColor.getAlpha();
-
-    // const invertedColorStr = invertColor(maskFill);
-    // const invertedColor = tinycolor(invertedColorStr);
-
     // 设置 alpha 透明度
     // 优先级 maskOpacity > maskFill自带opacity
     if (!isNil(maskOpacity)) {
       originColor.setAlpha(maskOpacity);
     }
-    //  else {
-    //   originColor.setAlpha(originOpacity);
-    // }
     return originColor.toHex8String();
   }, [maskFill, maskOpacity]);
   return (
